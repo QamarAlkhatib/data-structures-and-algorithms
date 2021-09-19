cf2f17dd73e4e3b2b46dd24066f36388c1e8e74e
Qamar <qalkhatib0@gmail.com>
2021-09-19 22:56:29 +0300

    finishing the challenge

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -73,7 +73,7 @@ addBirthYearProperty(octavia, 1947);
 console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
-const addBirthYearProperty = (obj, year) =>{
+const addBirthYearProperty = (obj, year) => {
   // Solution code here...
   obj.yearBorn = year;
 };
@@ -93,7 +93,8 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
-  people.forEach(value => {value.isAuthor = true;
+  people.forEach(value => {
+    value.isAuthor = true;
   });
 };
 
@@ -112,9 +113,9 @@ append(a, b);
 console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
-const append = (arr1, arr2) =>  arr1.push(...arr2);
-  // Solution code here...
- 
+const append = (arr1, arr2) => arr1.push(...arr2);
+// Solution code here...
+
 
 
 /* ------------------------------------------------------------------------------------------------
