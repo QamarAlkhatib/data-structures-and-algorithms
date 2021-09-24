96f21dfa044ed407f1a0c8cbdf6b4dbc278fe5d0
Qamar <qalkhatib0@gmail.com>
2021-09-25 00:12:18 +0300

    1-7 done

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -175,7 +175,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-describe('Testing challenge 6', () => {
+xdescribe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
